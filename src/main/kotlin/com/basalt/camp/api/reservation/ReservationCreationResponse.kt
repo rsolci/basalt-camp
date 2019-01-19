990b7d4aae22accc9f8b4dc8d1be6a7404d0f7ef
Rafael Solci <rsolci@gmail.com>
2019-01-19 19:37:55 -0200

    Validating dates

--- src/main/kotlin/com/basalt/camp/api/reservation/ReservationCreationResponse.kt
+++ src/main/kotlin/com/basalt/camp/api/reservation/ReservationCreationResponse.kt
@@ -6,4 +6,13 @@ class ReservationCreationResponse(success: Boolean,
                                   messages: List<String> = emptyList(),
                                   payload: ReservationCreationPayload? = null) : BaseResponse(success,
     messages,
-    payload)
+    payload) {
+
+    companion object {
+        fun emptySuccess() = ReservationCreationResponse(true, emptyList())
+    }
+
+    fun merge(response: ReservationCreationResponse): ReservationCreationResponse {
+        return ReservationCreationResponse(this.success && response.success, messages.union(response.messages).toList())
+    }
+}
--- src/main/kotlin/com/basalt/camp/business/reservation/ReservationService.kt
+++ src/main/kotlin/com/basalt/camp/business/reservation/ReservationService.kt
@@ -4,9 +4,8 @@ import com.basalt.camp.api.reservation.ReservationCreationPayload
 import com.basalt.camp.api.reservation.ReservationCreationRequest
 import com.basalt.camp.api.reservation.ReservationCreationResponse
 import com.basalt.camp.business.user.User
-import com.basalt.camp.helper.DateFormatter
 import org.springframework.stereotype.Service
-import java.text.ParseException
+import java.time.Duration
 import java.time.Instant
 import java.time.ZoneOffset
 import java.util.UUID
@@ -14,15 +13,22 @@ import java.util.UUID
 @Service
 class ReservationService {
 
-    fun createReservation(reservationCreationRequest: ReservationCreationRequest) : ReservationCreationResponse{
+    fun createReservation(reservationCreationRequest: ReservationCreationRequest): ReservationCreationResponse {
         val checkIn: Instant = reservationCreationRequest.checkIn.atTime(12, 0).atOffset(ZoneOffset.UTC).toInstant()
         val checkOut: Instant = reservationCreationRequest.checkOut.atTime(12, 0).atOffset(ZoneOffset.UTC).toInstant()
-        // TODO validate valid interval, in < out
+
+        var reservationCreationResponse = validateOrder(checkIn, checkOut)
+        reservationCreationResponse = validateReservationInterval(checkIn, checkOut).merge(reservationCreationResponse)
+
         if (reservationCreationRequest.name.isEmpty()) {
-            return ReservationCreationResponse(false, listOf("Name is required for reservation"))
+            reservationCreationResponse = reservationCreationResponse.merge(ReservationCreationResponse(false, listOf("Name is required for reservation")))
         }
         if (reservationCreationRequest.email.isEmpty()) {
-            return ReservationCreationResponse(false, listOf("E-mail is required for reservation"))
+            reservationCreationResponse = reservationCreationResponse.merge(ReservationCreationResponse(false, listOf("E-mail is required for reservation")))
+        }
+
+        if (!reservationCreationResponse.success) {
+            return reservationCreationResponse
         }
         // TODO check existing user
         val user =
@@ -36,5 +42,19 @@ class ReservationService {
         // TODO persist reservation
 
         return ReservationCreationResponse(success = true, payload = ReservationCreationPayload(reservation.id))
+    }
+
+    fun validateOrder(start: Instant, end: Instant): ReservationCreationResponse {
+        if (start.isAfter(end) || start == end) {
+            return ReservationCreationResponse(false, listOf("Check-out dat must be after check-in"))
+        }
+        return ReservationCreationResponse.emptySuccess()
+    }
+
+    fun validateReservationInterval(start: Instant, end: Instant): ReservationCreationResponse {
+        if (Duration.between(start, end).toDays() > 3) {
+            return ReservationCreationResponse(false, listOf("Maximum reservation period is 3 days"))
+        }
+        return ReservationCreationResponse.emptySuccess()
     }
 }
