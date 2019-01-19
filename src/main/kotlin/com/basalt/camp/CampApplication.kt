b4456b1ceb2a6c0028aa41bdeba2896d37e23799
Rafael Solci <rsolci@gmail.com>
2019-01-19 17:28:30 -0200

    Base app

---
+++ src/main/kotlin/com/basalt/camp/CampApplication.kt
@@ -0,0 +1,12 @@
+package com.basalt.camp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CampApplication
+
+fun main(args: Array<String>) {
+	runApplication<CampApplication>(*args)
+}
+
